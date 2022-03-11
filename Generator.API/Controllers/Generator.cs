3ad073c80e9857dd8ca52841a20e19fab1580103
alfredo-pacheco <alfredo.pacheco@wizeline.com>
2022-03-11 16:28:17 -0600

    use http and port 4321

--- Generator.API/Controllers/Generator.cs
+++ Generator.API/Controllers/Generator.cs
@@ -18,13 +18,13 @@ public class GeneratorController : ControllerBase
     {
         return Ok(ApplicationName);
     }
-   
-    [HttpPost, Route("/Generator/RunBackend")]
+
+    [HttpPost, Route("/Generator/RunBackend/{ApplicationName}")]
     public IActionResult RunBackend(string ApplicationName, bool Force)
     {
         return Ok(ApplicationName);
     }
-    
+
     [HttpPost, Route("/Generator/RunFrontend")]
     public IActionResult RunFrontend(string ApplicationName, bool Force)
     {
@@ -34,24 +34,24 @@ public class GeneratorController : ControllerBase
     [HttpPost, Route("/Generator/RunEntity/{ApplicationName}/{EntityName}")]
     public IActionResult RunEntity(string ApplicationName, string EntityName, bool Force)
     {
-        return Ok(new {ApplicationName, EntityName});
+        return Ok(new { ApplicationName, EntityName });
     }
 
     [HttpPost, Route("/Generator/RunPages/{ApplicationName}/{FrontendName}")]
     public IActionResult RunPages(string ApplicationName, string FrontendName, bool Force)
     {
-        return Ok(new {ApplicationName, FrontendName});
+        return Ok(new { ApplicationName, FrontendName });
     }
-    
+
     [HttpPost, Route("/Generator/RunComponent/{ApplicationName}/{ComponentName}")]
     public IActionResult RunComponent(string ApplicationName, string ComponentName, bool Force)
     {
-        return Ok(new {ApplicationName, ComponentName});
+        return Ok(new { ApplicationName, ComponentName });
     }
 
     [HttpPost, Route("/Generator/RunComponents/{ApplicationName}/{FrontendName}")]
     public IActionResult RunComponents(string ApplicationName, string FrontendName, bool Force)
     {
-        return Ok(new {ApplicationName, FrontendName});
+        return Ok(new { ApplicationName, FrontendName });
     }
 }
