72dddf36698b4ffd0cd0b6cc8958f8619e775e6c
juanfedo <juanfedo@yahoo.com>
2023-02-22 13:48:25 -0500

    Fixed order properties at serialized.

--- Generator.API/Definition/ComponentDefinition.cs
+++ Generator.API/Definition/ComponentDefinition.cs
@@ -1,42 +1,37 @@
 ﻿using System.Runtime.Serialization;
 
-namespace Generator.Definition;
-
-[DataContract]
-public class ComponentDefinition : HasYAML
+namespace Generator.Definition
 {
-    [DataMember(Order = 0)]
-    public string Name { get; set; } = null!;
-    
-    [DataMember(Order = 1)]
-    public string? Type { get; set; }
-    
-    [DataMember(Order = 2)]
-    public string? Entity { get; set; }
-    
-    [DataMember(Order = 3)]
-    public List<object> Layout = new List<object>();
-    
-    [DataMember(Order = 4)]
-    public Dictionary<object, object> Slots = new Dictionary<object, object>();
+    public class ComponentDefinition : HasYAML
+    {
+        public string Name { get; set; } = null!;
 
-    public EntryState EntryState { get; set; }
-}
+        public string? Type { get; set; }
 
-public class ComponentSlots
-{
-    public string? Dependencies { get; set; }
-    public string? Config { get; set; }
-    public string? Interface { get; set; }
-    public string? Ctor { get; set; }
-    public string? Load { get; set; }
-    public string? AfterLoad { get; set; }
-    public string? AfterCreate { get; set; }
-    public string? AfterSave { get; set; }
-    public string? BeforeSave { get; set; }
-    public string? OnChange { get; set; }
-    public string? Body { get; set; }
-    public string? Render { get; set; }
-    public string? AfterRemove { get; set; }
-    public string? OnOpenItem { get; set; }
+        public string? Entity { get; set; }
+
+        public List<object> Layout = new List<object>();
+
+        public Dictionary<object, object> Slots = new Dictionary<object, object>();
+
+        public EntryState EntryState { get; set; }
+    }
+
+    public class ComponentSlots
+    {
+        public string? Dependencies { get; set; }
+        public string? Config { get; set; }
+        public string? Interface { get; set; }
+        public string? Ctor { get; set; }
+        public string? Load { get; set; }
+        public string? AfterLoad { get; set; }
+        public string? AfterCreate { get; set; }
+        public string? AfterSave { get; set; }
+        public string? BeforeSave { get; set; }
+        public string? OnChange { get; set; }
+        public string? Body { get; set; }
+        public string? Render { get; set; }
+        public string? AfterRemove { get; set; }
+        public string? OnOpenItem { get; set; }
+    }
 }
--- Reusable/Utils/YAML.cs
+++ Reusable/Utils/YAML.cs
@@ -1,6 +1,5 @@
 ﻿using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
-using YamlDotNet.Serialization.TypeInspectors;
 
 namespace Reusable.Utils
 {
@@ -22,10 +21,8 @@ namespace Reusable.Utils
         {
             var serializer = new SerializerBuilder()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .WithTypeInspector(x => new SortedTypeInspector(x))
                 .Build();
-
-            // var serializer = YamlSerializer.Default;
-
             return serializer.Serialize(from);
         }
 
@@ -38,29 +35,5 @@ namespace Reusable.Utils
 
             return string.Join('\n', lines);
         }
-
-        public class SortedTypeInspector : TypeInspectorSkeleton
-        {
-            private readonly ITypeInspector _innerTypeInspector;
-
-            public SortedTypeInspector(ITypeInspector innerTypeInspector)
-            {
-                _innerTypeInspector = innerTypeInspector;
-            }
-
-            public override IEnumerable<IPropertyDescriptor> GetProperties(Type type, object container)
-            {
-                return _innerTypeInspector.GetProperties(type, container).OrderBy(x => x.Name);
-            }
-        }
-
-        public static class YamlSerializer
-        {
-            public static readonly Serializer Default =
-                (Serializer)new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance)
-                                       .WithTypeInspector(x => new SortedTypeInspector(x))
-                                       .Build();
-        }
-
     }
 }
